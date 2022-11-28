c22eb49dc69f50d746f4d8faee88b415283d4a1e
Sandro Simperl <sandrosimperl.cc@gmail.com>
2022-11-28 11:22:25 +0100

    updated

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,16 +1,14 @@
 package com.cc.java;
 public class App {
-    public static void main(String[] args) {
-    
+    public static void main( String[] args ) {
         Player player = new Player();
 
-        for (int i = 0; i < 12; i++) {
-            ausgabe(player.play());  
+        for ( int i = 0; i < 12; i++ ) {
+            ausgabe( player.play() );  
         }
-
     }
 
-    private static void ausgabe(String outStr) {
-        System.out.println(outStr);
+    private static void ausgabe( String outStr ) {
+        System.out.println( outStr );
     }
 }
--- com/cc/java/Level_3.java
+++ com/cc/java/Level_3.java
@@ -4,10 +4,13 @@ public class Level_3 {
     
     private String levelName = "Level 3";
     protected int points;
-
+    
     protected String play(){
         points++;
         return "Yahoo, " + levelName + " here, and I have " + points + " points!";
+    }
+    public int getPoints() {
+        return points;
     }
 
 }
